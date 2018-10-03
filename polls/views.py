58ca01e15932012b64f4e05c953dff9042f602b4
Seongeun Yu <johong130801@gmail.com>
2018-10-03 20:04:55 +0900

    tutorial 3 완료. 4에서 Generic View하기 전까지 작업함.

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from polls.models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -19,9 +20,30 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "질문의 답은 이것: %s"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'results.html', {'question': question})
+
+    # response = "질문의 답은 이것: %s"
+    # return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
-    return HttpResponse("여기에 투표하셨수: %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # POST 데이터가 올바르게 왔을 때만 redirect를 할 것임
+        # 이거는 데이터가 두번 들어가는걸 막아준다.
+        # 백버튼 눌른다던가 이런식으로 POST 두번 할 때...
+        return HttpResponseRedirect(
+            reverse('polls:results', args=(question_id,))
+        )
+
+    # return HttpResponse("여기에 투표하셨수: %s" % question_id)
